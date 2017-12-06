70f5631990f3636529c4ce1888e84a3d2163632d
khaledjabr <khaled.n.jabr@gmail.com>
2017-12-05 18:42:22 -0600

    baby ready to go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np 
 from sklearn.svm import SVR
+from sklearn.linear_model import Ridge 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from random import sample
@@ -8,30 +9,33 @@ from svr import JN_SVR
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 
-num_total = 100
-num_train = 60
-num_val = 20
-num_test = 20
 
-# indices of data in CSV (minus one to account for url being stripped)
 target_index = 59
-predictor_indices = [ *range(1,6), 10, *range(38, 59) ]
+predictor_indices = [ 11, *range(44, 59) ]
 
 #reading the csv file
 fileName = 'OnlineNewsPopularity.csv';
 news_articles = np.loadtxt(fileName, dtype = float, delimiter = ',', skiprows = 1, usecols=range(1,61) )
 
+#getting feature names 
+all_features = []
+with open(fileName, 'r') as f:
+    all_features = f.readline().split(',')
+
+predictors = [all_features[i] for i in predictor_indices]
 
 # data
 data = news_articles[:,predictor_indices]
 target = news_articles[:, target_index]
 
 #splitting 
-tr_data = data[:int(0.8*len(data))]
-tr_target = target[:len(tr_data)]
+# extract test data
+te_indices = sample(range(len(data)), int(0.2*len(data)))
+te_data = data[te_indices]
+te_target = target[te_indices]
 
-te_data = data[len(tr_data):]
-te_target = target[len(tr_data):]
+tr_data = np.delete(data, te_indices, 0)
+tr_target = np.delete(target, te_indices, 0)
 
 # print("Training " , len(tr_data) , "Testing " , len(te_data))
 # print(len(tr_data) + len(te_data) == len(data))
@@ -39,10 +43,21 @@ te_target = target[len(tr_data):]
 # print("Training " , len(tr_target) , "Testing " , len(te_target))
 # print(len(tr_target) + len(te_target) == len(target))
 
+# #Plot stuff yo
+# t = np.arange(0.0, 2.0, 0.01)  
+# s = np.sin(2*np.pi*t)  
+
+# plt.plot(t, s)  
+# plt.xlabel('time (s)')  
+# plt.ylabel('voltage (mV)')  
+# plt.title('voltage (mV) vs. time (sec)')   
+# plt.grid(True)  
+# plt.savefig("plot-voltage-vs.-time.png")  
+# plt.show()  
 
 
 
-
+#SVR
 #USING OUR OWN IMPLEMENTATION
 SVR_MODEL = JN_SVR(2 , 0.001)
 SVR_MODEL.fit(tr_data , tr_target)
@@ -53,7 +68,6 @@ svr_poly = SVR(kernel='poly', C=1e3, degree=2)
 y_poly = svr_poly.fit(tr_data,tr_target).predict(te_data)
 #MEASURING ERROR 
 print("SUPPORT VECTOR REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
-
 print("MEAN SQUARED ERROR")
 print("Uing Scikit Learn : " , mean_squared_error(te_target , y_poly))
 print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, y_p))
@@ -63,40 +77,70 @@ print("Uing Scikit Learn : " , mean_absolute_error(te_target , y_poly))
 print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, y_p))
 
 
-# #pair wise plots
-# fig, axes = plt.subplots(ncols=3)
-# for i, yvar in enumerate([1, 2, 3]):
-#     axes[i].scatter(tr_data[:,i],tr_target)
-
-# num_total = len(data)
-# num_val = num_total // 5
-# num_te = num_total // 5
-# num_tr = num_total - num_val - num_te
-
-# print("", str(num_tr), str(num_val), str(num_te))
-
-# # extract test data
-# te_indices = sample(range(len(data)), num_te)
-# te_data = data[te_indices]
-# te_target = target[te_indices]
-
-# data = np.delete(data, te_indices, 0)
-# target = np.delete(target, te_indices, 0)
-
-# # extract validation data
-# v_indices = sample(range(len(data)), num_val)
-# v_data = data[v_indices]
-# v_target = target[v_indices]
-# # rest is for training
-# tr_data = np.delete(data, v_indices, 0)
-# tr_target = np.delete(target, v_indices, 0)
-
-# print("Data: ", str(len(tr_data)), str(len(v_data)), str(len(te_data)))
-# print("Target: ", str(len(tr_target)), str(len(v_target)), str(len(te_target)))
-# print("test shape : \n" ,  te_target.shape)
-# print("test: \n" ,  te_target)
 
 
 
 
-#done
+
+#Ridge 
+
+#using our implementation 
+alpha = 0.01
+our_ridge = JN_Ridge(alpha=0.001)
+our_ridge.fit(tr_data, tr_target)
+our_ridge_predicted = our_ridge.predict(te_data)
+
+#using scikit learn 
+ridge_clf = Ridge(alpha = 0.001).fit(tr_data , tr_target)
+sklearn_ridge_prediction = ridge_clf.predict(te_data)
+
+print("RIDGE REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
+print("MEAN SQUARED ERROR (MSE)")
+print("Uing Scikit Learn : " , mean_squared_error(te_target , sklearn_ridge_prediction))
+print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, our_ridge_predicted))
+
+print("MEAN ABSOLUTE ERROR (MAE)")
+print("Uing Scikit Learn : " , mean_absolute_error(te_target , sklearn_ridge_prediction))
+print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, our_ridge_predicted))
+
+
+
+
+
+#LASSO REGRESSION
+#our implementation
+our_lasso = JN_Lasso(alpha=0.01)
+our_lasso.fit(tr_data, tr_target)
+our_lasso_predicted = our_lasso.predict(te_data)
+
+#sklearn implementation
+sklearn_lasso= Lasso(alpha=0.01, copy_X=True, normalize=True, max_iter=1000).fit(tr_data, tr_target)
+sklearn_lasso_predicted = sklearn_lasso.predict(te_data)
+
+
+print("LASSO REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
+print("MEAN SQUARED ERROR (MSE)")
+print("Uing Scikit Learn : " , mean_squared_error(te_target , sklearn_lasso_predicted))
+print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, our_lasso_predicted))
+
+print("MEAN ABSOLUTE ERROR (MAE)")
+print("Uing Scikit Learn : " , mean_absolute_error(te_target , sklearn_lasso_predicted))
+print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, our_lasso_predicted))
+
+
+
+
+
+def makePlot(feature_number , actual , our_p , scikit_p, algorithm): 
+	x = te_data[:,feature_number]
+	plt.scatter( x , actual , color ='blue' ,label ='Actual Number of shares')
+	plt.plot(x , our_p , color='red'  , label ='OUR ' +  algorithm)
+	plt.plot(x , scikit_p , color='red'  , label ='SKLEARN' +  algorithm)
+	plt.plot(te_data [;,feature_number] , )
+	plt.xlabel(predictors[feature_number])
+	plt.ylabel("Number of Shares")
+	plt.legend()
+	plt.title( "Number of Shares  vs " str(predictors[feature_number]))  
+	plt.show()
+
+#done 
