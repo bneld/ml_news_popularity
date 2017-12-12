fb8ad5f536d6b28754b70fceda61a62988168bdb
khaledjabr <khaled.n.jabr@gmail.com>
2017-12-11 18:18:04 -0600

    SVR ready to go

--- main.py
+++ main.py
@@ -31,92 +31,98 @@ target = news_articles[:, target_index]
 #splitting 
 # extract test data
 te_indices = sample(range(len(data)), int(0.2*len(data)))
-te_data = data[te_indices]
-te_target = target[te_indices]
+testing_data = data[te_indices]
+testing_data_target = target[te_indices] 
 
 
-tr_data = np.delete(data, te_indices, 0)
-tr_target = np.delete(target, te_indices, 0)
+te_data = testing_data[:40]
+te_target = testing_data_target[:40]
 
 
+training_data = np.delete(data, te_indices, 0)
+testing_data_target = np.delete(target, te_indices, 0)
+
+tr_data = training_data[:200]
+tr_target = testing_data_target[:200]
 
 
+# 
 #SVR
 #USING OUR OWN IMPLEMENTATION
-SVR_MODEL = JN_SVR(2 , 0.001)
+SVR_MODEL = JN_SVR(2 , 0.1)
 SVR_MODEL.fit(tr_data , tr_target)
 y_p = SVR_MODEL.predict(te_data)
 
 #USING SCIKIT LEARN 
 svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-y_poly = svr_poly.fit(tr_data,tr_target).predict(te_data)
+y_poly = svr_poly.fit(tr_data , tr_target).predict(te_data)
 #MEASURING ERROR 
 print("SUPPORT VECTOR REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
 print("MEAN SQUARED ERROR")
 print("Uing Scikit Learn : " , mean_squared_error(te_target , y_poly))
-print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, y_p))
+print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, y_p[1:]))
 
 print("MEAN ABSOLUTE ERROR")
 print("Uing Scikit Learn : " , mean_absolute_error(te_target , y_poly))
-print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, y_p))
+print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, y_p[1:]))
 
 
-#Ridge 
-#using our implementation 
-alpha = 0.01
-our_ridge = JN_Ridge(alpha=0.001)
-our_ridge.fit(tr_data, tr_target)
-our_ridge_predicted = our_ridge.predict(te_data)
+# #Ridge 
+# #using our implementation 
+# alpha = 0.01
+# our_ridge = JN_Ridge(alpha=0.001)
+# our_ridge.fit(tr_data, tr_target)
+# our_ridge_predicted = our_ridge.predict(te_data)
 
-#using scikit learn 
-ridge_clf = Ridge(alpha = 0.001).fit(tr_data , tr_target)
-sklearn_ridge_prediction = ridge_clf.predict(te_data)
+# #using scikit learn 
+# ridge_clf = Ridge(alpha = 0.001).fit(tr_data , tr_target)
+# sklearn_ridge_prediction = ridge_clf.predict(te_data)
 
-print("RIDGE REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
-print("MEAN SQUARED ERROR (MSE)")
-print("Uing Scikit Learn : " , mean_squared_error(te_target , sklearn_ridge_prediction))
-print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, our_ridge_predicted))
+# print("RIDGE REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
+# print("MEAN SQUARED ERROR (MSE)")
+# print("Uing Scikit Learn : " , mean_squared_error(te_target , sklearn_ridge_prediction))
+# print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, our_ridge_predicted))
 
-print("MEAN ABSOLUTE ERROR (MAE)")
-print("Uing Scikit Learn : " , mean_absolute_error(te_target , sklearn_ridge_prediction))
-print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, our_ridge_predicted))
-
-
-
-
-
-#LASSO REGRESSION
-#our implementation
-our_lasso = JN_Lasso(alpha=0.01)
-our_lasso.fit(tr_data, tr_target)
-our_lasso_predicted = our_lasso.predict(te_data)
-
-#sklearn implementation
-sklearn_lasso= Lasso(alpha=0.01, copy_X=True, normalize=True, max_iter=1000).fit(tr_data, tr_target)
-sklearn_lasso_predicted = sklearn_lasso.predict(te_data)
-
-
-print("LASSO REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
-print("MEAN SQUARED ERROR (MSE)")
-print("Uing Scikit Learn : " , mean_squared_error(te_target , sklearn_lasso_predicted))
-print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, our_lasso_predicted))
-
-print("MEAN ABSOLUTE ERROR (MAE)")
-print("Uing Scikit Learn : " , mean_absolute_error(te_target , sklearn_lasso_predicted))
-print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, our_lasso_predicted))
+# print("MEAN ABSOLUTE ERROR (MAE)")
+# print("Uing Scikit Learn : " , mean_absolute_error(te_target , sklearn_ridge_prediction))
+# print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, our_ridge_predicted))
 
 
 
+
+
+# #LASSO REGRESSION
+# #our implementation
+# our_lasso = JN_Lasso(alpha=0.01)
+# our_lasso.fit(tr_data, tr_target)
+# our_lasso_predicted = our_lasso.predict(te_data)
+
+# #sklearn implementation
+# sklearn_lasso= Lasso(alpha=0.01, copy_X=True, normalize=True, max_iter=1000).fit(tr_data, tr_target)
+# sklearn_lasso_predicted = sklearn_lasso.predict(te_data)
+
+
+# print("LASSO REGRESSION: ON TESTING DATA\n++++++++++++++++++++++++++++++++++++++++\n")
+# print("MEAN SQUARED ERROR (MSE)")
+# print("Uing Scikit Learn : " , mean_squared_error(te_target , sklearn_lasso_predicted))
+# print("Uing OUR IMPLEMENTATION : " , mean_squared_error( te_target, our_lasso_predicted))
+
+# print("MEAN ABSOLUTE ERROR (MAE)")
+# print("Uing Scikit Learn : " , mean_absolute_error(te_target , sklearn_lasso_predicted))
+# print("Uing OUR IMPLEMENTATION : " , mean_absolute_error(te_target, our_lasso_predicted))
 
 
 def makePlot(feature_number , actual , our_p , scikit_p, algorithm): 
 	x = te_data[:,feature_number]
-	plt.scatter( x , actual , color ='blue' ,label ='Actual Number of shares')
-	plt.plot(x , our_p , color='red'  , label ='OUR ' +  algorithm)
+	print(x.shape)
+	print(actual.shape)
+	plt.scatter( x , te_target , color ='orange' ,label ='Actual Number of shares')
+	plt.plot(x , our_p[1:] , color='blue'  , label ='OUR ' +  algorithm)
 	plt.plot(x , scikit_p , color='red'  , label ='SKLEARN' +  algorithm)
 	plt.xlabel(predictors[feature_number])
 	plt.ylabel("Number of Shares")
 	plt.legend()
 	plt.title( "Number of Shares  vs " + str(predictors[feature_number]))  
 	plt.show()
+	plt.gcf().clear()
 
--- svr.py
+++ svr.py
@@ -1,34 +1,51 @@
 import numpy as np
 from numpy.linalg import inv
 class JN_SVR: 
-	def __init__(self, deg , r) :
+	def __init__(self, deg , r=1000 ) :
+		# print("RRRRR : ", r )
 		self.reg = r 
 		self.degree = deg
+		self.training_data = []
 		self.alphas = []
 
 	def calc_K_Mat(self, input_mat, degree, c ) : 
-	    k_mat = np.ones([input_mat.shape[0]+1 ,input_mat.shape[0]+1 ])
-	    k_mat[0][0] = 0
-	    i_matrix = np.identity(input_mat.shape[0])
-	    for i in range(1, k_mat.shape[0]) : 
-	        for j in range (1, k_mat.shape[0]) : 
-	            #Kernal  
-	            print("K Matrix ( " + str(i) + " , " + str(j) +") ")
-	            k = (np.sum((input_mat[i-1:i , : ]).T * input_mat[j-1:j , :]) ** degree)  + (1/c) * i_matrix[i-1][j-1]
-	#             k = ((np.sum((input_mat[i-1:i , : ]).T * input_mat[j-1:j , :]))  ** degree)  
-	            k_mat[i][j] = k
-	    return k_mat
+		k_mat = np.ones([input_mat.shape[0]+1 ,input_mat.shape[0]+1 ])
+		k_mat[0][0] = 0
+		i_matrix = np.identity(input_mat.shape[0])
+		for i in range(1, k_mat.shape[0]) : 
+		    for j in range (1, k_mat.shape[0]) : 
+		    	print("Fitting : K Matrix ( {} , {} )".format(i , j))
+		    	k = (np.sum((input_mat[i-1:i , : ]).T * input_mat[j-1:j , :]) ** degree)  + (1/c) * i_matrix[i-1][j-1]
+		    	k_mat[i][j] = k
+		return k_mat
 
 	def fit(self, input_data , target) : 
+		self.training_data = np.copy(input_data)
+		# print("Training data shape : " , self.training_data.shape)
 		k_matrix = self.calc_K_Mat(input_data ,self.degree , self.reg)
 		target = np.insert(target, 0 , 0)
 		p1 = inv(np.matmul(k_matrix.T , k_matrix ))
 		p2 = np.matmul( k_matrix.T , target)
 		alphas = np.matmul(p1 ,p2 )
 		self.alphas = alphas
-	def predict(self , input_data) : 
-	    k_mat= self.calc_K_Mat(input_data , self.degree ,self.reg ) 
-	    return np.matmul(self.alphas[1:].T , k_mat[1: , 1:]) + self.alphas[0]
 
+	def calc_K_matrix_input(self, input_data ): 
+		input_k_matrix = np.ones([input_data.shape[0] +1 , self.training_data.shape[0] + 1] )
+		input_k_matrix[0][0] = 0 
+		for i in range(1, input_k_matrix.shape[0]):
+			for j in range(1, input_k_matrix.shape[1]):
+				print("Predicting : K Matrix ( {} , {} )".format(i , j))
+				input_k_matrix[i][j] = (np.sum((input_data[i-1:i , : ]).T * self.training_data[j-1:j , :] ) ** self.degree)
+
+		return input_k_matrix
+
+		return []
+	def predict(self , input_data) : 
+		# print(self.reg)
+		# print(str(1e3))
+		k_mat= self.calc_K_matrix_input(input_data)
+		# print(k_mat.shape)
+		# return np.matmul( k_mat[1: , 1:], self.alphas[1:]) + self.alphas[0]
+		return np.matmul( k_mat, self.alphas)
 
 
